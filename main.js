78b5b0d71ab4f0841055c5ab56646e391fadb437
tanookibruv <erendon1836@gmail.com>
2021-09-12 22:01:38 -0700

    got rows to change colors based off time

--- main.js
+++ main.js
@@ -1,22 +1,49 @@
-//Variables using moment to grab today's date.
+//Variables using moment to grab today's date and fill in header.
 const today = moment().format('dddd, MMMM Do YYYY');
 var currentDayEl = document.getElementById('currentDay')
 currentDayEl.innerHTML = today;
 
-//Variable to grab time by the hour for daily planner
-var hourTime = moment().format('h A');
+//Variable for current time and timestamps on rows for overall function
+var currentTime = moment().format('ha');
+var hourEls = document.querySelectorAll('.hour');
 
-function pastTime() {
+//Variables to pastEl, futureEl, and presentEl rows. textarea and saveBtn
+var pastEl = document.querySelectorAll('.past');
+var presentEl = document.querySelectorAll('.present');
+var futureEl = document.querySelectorAll('.future');
+var textEl = document.querySelectorAll('.mytext')
+var saveEl = document.querySelectorAll('.saveBtn')
+
+
+
+// function to compare current time to daily planner
+function matchTime() {
     var hourEls = document.querySelectorAll('.hour');
-    var rowEl = document.querySelectorAll('.row')
     hourEls.forEach(element => {
         element.textContent;
         
-        if (element <= hourTime) {
-            rowEl.classList.add("past")
-        }
-        console.log(element.parentNode)
-    });
-}
+        if (currentTime !== element.textContent) {
+            if (currentTime > element.textContent) {
+                console.log('ct > tt')
+                element.parentNode.classList.add('past')
+            } else {
+                console.log('ct < tt')
+                element.parentNode.classList.add('future')
+            }
 
-window.onload = pastTime();
+        } else {
+            if (element.textContent === currentTime) {
+                console.log('time is current')
+                element.parentNode.classList.add('present')
+            }
+        }
+    });
+};
+
+
+
+
+
+
+window.onload = matchTime();
+
